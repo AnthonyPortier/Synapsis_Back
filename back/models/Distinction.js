7771cf96bfa0604845129d0e029bac600bdda1ca
Ouahid Aqodad <aqodad.ouahid26@gmail.com>
2019-12-18 20:00:38 +0100

    add distinction model

---
+++ back/models/Distinction.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    const Distinction = sequelize.define('Distinction', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date: {
+            type: DataTypes.DATE,
+            validate: {
+                isDate: true
+            },
+            allowNull: false,
+        },
+    });
+
+    Distinction.associate = function (models) {
+
+
+    };
+    return Distinction;
+};
